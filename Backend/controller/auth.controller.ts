5808ce7ffad49843aeb490f390edcf6873156835
Miqael-Dev <miqael.dev@gmail.com>
2024-05-25 02:03:00 +0000

    create login route

--- Backend/controller/auth.controller.ts
+++ Backend/controller/auth.controller.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
 
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const prisma = new PrismaClient();
 
+interface LoginRequetBody {
+  email: string;
+  password: string;
+}
 interface SignupRequestBody {
   username: string;
   email: string;
@@ -27,5 +33,20 @@ export const Signup: RequestHandler<{}, {}, SignupRequestBody> = async (
     res.status(200).json(register);
   } catch (error) {
     res.status(500).json({ message: "user already exists" });
+  }
+};
+
+export const Login: RequestHandler<{}, {}, LoginRequetBody> = async (
+  req,
+  res
+) => {
+  let { email, password } = req.body;
+
+  let user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    return res.status(500).json({ message: "user not registered" });
   }
 };
--- Backend/routes/auth.route.ts
+++ Backend/routes/auth.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { Signup } from "../controller/auth.controller";
+import { Signup, Login } from "../controller/auth.controller";
 const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 
 module.exports = router;
